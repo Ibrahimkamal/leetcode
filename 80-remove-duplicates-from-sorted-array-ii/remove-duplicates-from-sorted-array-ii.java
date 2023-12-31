08e42303bff5f4aea202d58c5c51e7abb95e1ae9
Ibrahim Kamal <Ibrahim_kamal@outlook.com>
2023-12-31 19:25:19 +0400

    Time: 2 ms (11.60%) | Memory: 44.1 MB (23.61%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
@@ -1,34 +1,32 @@
 class Solution {
-    private void shift(int[]nums,int i)
+    public int [] shiftLeft(int[]nums,int index)
     {
-        for(int j=i+1;j<nums.length;j++)
+        for(int j=index+1;j<nums.length;j++)
         {
             nums[j-1]=nums[j];
         }
+        return nums;
     }
     public int removeDuplicates(int[] nums) {
-        int ptr=1;
         int i=1;
         int length=nums.length;
         int count=1;
         while(i<length)
         {
-            if(nums[i-1]==nums[i])
+            if(nums[i]==nums[i-1])
             {
                 count++;
                 if(count>2)
                 {
-                    this.shift(nums, i);
+                    this.shiftLeft(nums, i);
                     i--;
                     length--;
                 }
-                
             }else{
-                    count=1;
+                count=1;
             }
             i++;
         }
         return length;
-        
     }
 }
