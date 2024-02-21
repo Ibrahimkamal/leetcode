7dc9a9a3522f56bbb10c2b09235cc28634e3caef
Ibrahim Kamal <Ibrahim_kamal@outlook.com>
2024-02-21 10:22:13 +0200

    Time: 29 ms (18.78%) | Memory: 50.5 MB (77.37%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -3,25 +3,24 @@ class Solution {
     private String text1;
     private String text2;
     private int dp(int index1, int index2) {
-        // int m=text1.length();
-        // int n=text2.length();
-        // if (index1 > m || index2 > n) {
-        //     return 0;
-        // }
-        if (cache[index1][index2]==-1)
+        if (cache[index1][index2]!=-1)
         {
-            int option1=dp(index1+1,index2);
-
-            int firstOccurance=text2.indexOf(text1.charAt(index1),index2);
-            int option2=0;
-
-            if(firstOccurance!=-1)
-            {
-                option2=1+dp(index1+1,firstOccurance+1);
-            }
-            cache[index1][index2]=Math.max(option1, option2);
-            
+            return cache[index1][index2];
         }
+        int answer=0;
+        if (text1.charAt(index1)==text2.charAt(index2))
+        {
+            answer=1+ dp(index1+1,index2+1);
+
+        }else{
+            int val1=dp(index1+1,index2);
+            int val2=dp(index1,index2+1);
+
+            answer=Math.max(val1, val2);
+        }
+        
+        cache[index1][index2]=answer;
+        
         return cache[index1][index2];
     }
 
