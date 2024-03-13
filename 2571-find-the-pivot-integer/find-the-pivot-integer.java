fddcb8752077c06eae034d66e77af5caee3d6c36
Ibrahim Kamal <Ibrahim_kamal@outlook.com>
2024-03-13 09:34:34 +0200

    Time: 1 ms (77.54%) | Memory: 40.4 MB (67.67%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.java
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.java
@@ -1,9 +1,7 @@
 class Solution {
     public int pivotInteger(int n) {
         int total_sum = 0;
-        for (int i = 0; i <= n; i++) {
-            total_sum += i;
-        }
+        total_sum=n*(n+1)/2;
         int current_sum=0;
         for (int i = 0; i <= n; i++) {
             current_sum += i;
