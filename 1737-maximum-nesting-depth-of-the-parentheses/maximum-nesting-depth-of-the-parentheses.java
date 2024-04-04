fd56ead587f93c06eaf67c565f991140054ac77d
Ibrahim Kamal <Ibrahim_kamal@outlook.com>
2024-04-04 11:46:39 +0400

    Time: 0 ms (100.00%) | Memory: 41.3 MB (74.86%) - LeetSync

--- 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.java
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.java
@@ -1,7 +1,5 @@
 class Solution {
     public int maxDepth(String s) {
-        Set<Character> set = Set.of('+', '-', '*', '/', '(', ')');
-        Stack<Character> stack=new Stack<>();
         int count=0;
         int max=0;
         for(int i=0;i<s.length();i++){
@@ -12,10 +10,6 @@ class Solution {
                 count--;
             }
             max=Math.max(max,count);
-
-            // if (count<0){
-            //     return 0;
-            // }
         }
         return max;
     }
