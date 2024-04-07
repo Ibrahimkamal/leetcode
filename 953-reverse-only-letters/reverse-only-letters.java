7314634ca005d42e6621fbf80704467c17a521a1
Ibrahim Kamal <Ibrahim_kamal@outlook.com>
2024-04-07 22:57:07 +0400

    Time: 6 ms (10.20%) | Memory: 41.8 MB (18.61%) - LeetSync

---
+++ 953-reverse-only-letters/reverse-only-letters.java
@@ -0,0 +1,34 @@
+
+class Solution {
+    private boolean isCharacter(char c) {
+        if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z') {
+            return true;
+        }
+        return false;
+    }
+
+    public String reverseOnlyLetters(String s) {
+        int start=0;
+        int end=s.length()-1;
+        char[] arr=s.toCharArray();
+        while(start<=end){
+            if(isCharacter(arr[start]) && isCharacter(arr[end]))
+            {
+                char temp=arr[start];
+                arr[start]=arr[end];
+                arr[end]=temp;
+                start++;
+                end--;
+            }else if(!isCharacter(arr[start])){
+                start++;
+            }else if(!isCharacter(arr[end])){
+                end--;
+            }
+        }
+        String result="";
+        for(char c:arr){
+            result+=c;
+        }
+        return result;
+    }
+}
