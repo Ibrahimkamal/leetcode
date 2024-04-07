9998cf98a971e0e99ba863b8e61833baf1c5c35a
Ibrahim Kamal <Ibrahim_kamal@outlook.com>
2024-04-07 13:58:20 +0400

    Time: 9 ms (43.48%) | Memory: 42.1 MB (98.35%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,13 +1,10 @@
 class Solution {
     public String reverseWords(String s) {
-        String[] splited_string=s.split("\\s+");
+        String[] splited_string=s.trim().split("\\s+");
         ArrayDeque <String> stack=new ArrayDeque<>();
         for(String temp:splited_string){
-            if (temp!=""){
-                stack.push(temp);
-            }
+            stack.push(temp);
         }
-
         return String.join(" ",stack);
     }
 }
