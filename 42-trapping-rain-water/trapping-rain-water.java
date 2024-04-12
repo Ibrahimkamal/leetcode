22f7ba3f6fff8007b112211b45a8a9ce4a71c961
Ibrahim Kamal <Ibrahim_kamal@outlook.com>
2024-04-12 10:44:43 +0400

    Time: 670 ms (5.01%) | Memory: 45.8 MB (70.22%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int trap(int[] height) {
+        
+        int answer=0;
+        for(int i=0;i<height.length;i++){
+            int left_max=0;
+            int right_max=0;
+            for(int j=i;j>=0;j--){
+                left_max=Math.max(left_max, height[j]);
+
+            }
+            for(int j=i;j<height.length;j++){
+                right_max=Math.max(right_max, height[j]);
+            }
+            answer+=Math.min(left_max,right_max)-height[i];
+        }
+        return answer;
+    }
+}
