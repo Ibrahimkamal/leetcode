b1a56de73f2a2a18bb98d025ba6f1381178fe0ba
Ibrahim Kamal <Ibrahim_kamal@outlook.com>
2024-07-24 23:02:27 +0300

    Time: 1 ms (99.98%) | Memory: 45.4 MB (77.05%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,25 +1,11 @@
-class Solution {
-    int[] nums;
-    Boolean[]cache;
-    private boolean dp(int index){
-        if(index==nums.length-1)
-            return true;
-        else if(index>nums.length-1)
-            return false;
-        if(this.cache[index]!=null)
-            return this.cache[index];
-        for(int i=1;i<=nums[index];i++){
-            if(dp(index+i))
-                return true;
-            else
-                this.cache[index]=false;
-        }
-        return false;
-    }
+public class Solution {
     public boolean canJump(int[] nums) {
-        this.nums=nums;
-        cache=new Boolean[nums.length];
-        
-        return dp(0);
+        int lastPos = nums.length - 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (i + nums[i] >= lastPos) {
+                lastPos = i;
+            }
+        }
+        return lastPos == 0;
     }
 }
