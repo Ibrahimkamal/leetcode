f36df38d3d52387d631ec34a72fba654ad8a6a9e
Ibrahim Kamal <Ibrahim_kamal@outlook.com>
2024-07-26 18:17:18 +0300

    Time: 7 ms (17.81%) | Memory: 44.3 MB (10.93%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -2,9 +2,22 @@ class Solution {
     public boolean isPalindrome(int x) {
         if (x<0)
             return false;
-        String str_temp=Integer.toString(x);
-        for(int i=0;i<str_temp.length()/2;i++){
-            if(str_temp.charAt(i)!=str_temp.charAt(str_temp.length()-1-i))
+        // String str_temp=Integer.toString(x);
+        // for(int i=0;i<str_temp.length()/2;i++){
+        //     if(str_temp.charAt(i)!=str_temp.charAt(str_temp.length()-1-i))
+        //         return false;
+        // }
+        // return true;
+        ArrayDeque<Integer> dequeue=new ArrayDeque<>();
+        while(x>0){
+            dequeue.add(x%10);
+            x=x/10;
+        }
+        while(dequeue.size()>1){
+            if(dequeue.peekFirst()==dequeue.peekLast()){
+                dequeue.removeFirst();
+                dequeue.removeLast();
+            }else
                 return false;
         }
         return true;
