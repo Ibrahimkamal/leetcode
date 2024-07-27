ac55e5d1e938cb196a65c15bec608282fc0e3ec9
Ibrahim Kamal <Ibrahim_kamal@outlook.com>
2024-07-27 14:32:00 +0300

    Time: 2 ms (10.51%) | Memory: 40.5 MB (56.62%) - LeetSync

--- 172-factorial-trailing-zeroes/factorial-trailing-zeroes.java
+++ 172-factorial-trailing-zeroes/factorial-trailing-zeroes.java
@@ -2,10 +2,10 @@ class Solution {
     public int trailingZeroes(int n) {
         int zeroCount = 0;
         for (int i = 5; i <= n; i += 5) {
-            int currentFactor = i;
-            while (currentFactor % 5 == 0) {
-                zeroCount++;
-                currentFactor /= 5;
+            int powerOf5 = 5;
+            while (i % powerOf5 == 0) {
+                zeroCount += 1;
+                powerOf5 *= 5;
             }
         }
         return zeroCount;
