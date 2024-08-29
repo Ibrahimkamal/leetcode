ed77a375a48bc40209c5ba4420a42f0d3c4d22f4
Ibrahim Kamal <Ibrahim_kamal@outlook.com>
2024-08-29 23:59:38 +0200

    Time: 1110 ms (12.27%) | Memory: 28 MB (65.24%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -1,10 +1,4 @@
 class Solution:
-    def rotateOnce(self, nums):
-        last = nums[len(nums) - 1]
-        for i in range(len(nums)-2, -1,-1):
-            nums[i + 1] = nums[i]
-        nums[0] = last
-
     def rotate(self, nums: List[int], k: int) -> None:
         """
         Do not return anything, modify nums in-place instead.
