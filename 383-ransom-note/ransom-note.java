00e8101ce1a682a178218cb6099b324ba77b9670
Ibrahim Kamal <Ibrahim_kamal@outlook.com>
2024-09-21 13:16:56 +0400

    Time: 16 ms (19.30%) | Memory: 45 MB (42.38%) - LeetSync

--- 383-ransom-note/ransom-note.java
+++ 383-ransom-note/ransom-note.java
@@ -1,31 +1,17 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        // if(ransomNote.length()>magazine.length())
-        // {
-        //     return false;
-        // }
-        Map<Character,Integer> map=new HashMap<>();
-        for(int i=0;i<magazine.length();i++)
-        {
-           char c=magazine.charAt(i);
-           map.put(c,map.getOrDefault(c,0)+1);
+        HashMap<Character,Integer> map=new HashMap<>();
+        for(int i=0;i<magazine.length();i++){
+            map.put(magazine.charAt(i),map.getOrDefault(magazine.charAt(i),0)+1);
         }
-        for(int i=0;i<ransomNote.length();i++)
-        {
-            char c=ransomNote.charAt(i);
-            if(!map.containsKey(c)){
+        for(int i=0;i<ransomNote.length();i++){
+            if (!map.containsKey(ransomNote.charAt(i)) || map.get(ransomNote.charAt(i))==0){
                 return false;
             }
-            map.put(c,map.get(c)-1);
-        }
-        for(int count:map.values())
-        {
-            if(count<0)
-            {
-                return false;
+            else{
+                map.put(ransomNote.charAt(i),map.get(ransomNote.charAt(i))-1);
             }
         }
         return true;
-
     }
 }
